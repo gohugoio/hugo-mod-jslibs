ee2c595f2dff0d3c4d0d9f1db3df8efedd274072
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-10-31 16:18:06 +0100

    instantclick: Fix init (take 2)
    Now tested.

---
+++ instantclick/assets/jslibs/instantclick/index.js
@@ -0,0 +1,7 @@
+/* 
+See http://instantclick.io/download
+
+To make it more flexible for the end user, we need to rework this, but for now, we'll just use the default settings.
+
+*/
+InstantClick.init();
